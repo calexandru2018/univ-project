9f11c06db9582e35e4d89f1d145670128f4b4696
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2019-10-20 16:19:26 +0200

    Add project files.

---
+++ Library/Models/Author.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Book> booksWritten { get; set; }
+
+        /// <summary>
+        /// Useful for adding the book objects directly to a ListBox.
+        /// </summary>
+        public override string ToString()
+        {
+            return String.Format("[{0}] -- {1}", this.Id, this.Name);
+        }
+    }
+}
---
+++ Library/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models {
+    public class Book {
+
+        public int Id { get; set; }
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        //public Author author { get; set; }
+        //public ICollection<BookCopy> copies { get; set; }
+
+        /// <summary>
+        /// Useful for adding the book objects directly to a ListBox.
+        /// </summary>
+        public override string ToString() {
+            return String.Format($"[{this.Id}] -- {this.Title}");
+        }
+    }
+}
